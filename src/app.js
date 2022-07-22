6b26b947b118f10323d5c0a1bec583a1bd1d6fd0
Nayara <nrdeandrade@gmail.com>
2022-07-22 13:06:03 -0300

    initial commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import chalk from 'chalk';
+import connection from './database/database.js';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.listen(process.env.PORT, () => {
+    console.log(chalk.bold.bgRed(`Server is listening on port ${process.env.PORT}`));
+});
