02bde4030eb4b8ba72bfbbc1e49ef11a0ab52394
Nayara <nrdeandrade@gmail.com>
2022-07-24 16:57:04 -0300

    feat: add query string option on get games route

--- src/controllers/gamesControllers.js
+++ src/controllers/gamesControllers.js
@@ -1,14 +1,24 @@
 import connection from '../database/database.js';
 
 export async function getGames(req, res) {
+    const name = req.query.name;
+    const defaultQuery = `
+    SELECT games.*, categories.name as "categoryName" 
+    FROM games
+    JOIN categories
+    ON games."categoryId"=categories.id
+    `
     try {
-        const games = await connection.query(`
-        SELECT games.*, categories.name as "categoryName" 
-        FROM games
-        JOIN categories
-        ON games."categoryId"=categories.id
-        `)
-        return res.status(200).send(games.rows)
+        if(name) {
+            const { rows: games } = await connection.query(`
+            ${defaultQuery}
+            WHERE lower(games.name) LIKE $1
+            OR games.name LIKE $1
+            `, [`%${name}%`]);
+            return res.send(games)
+        }
+        const { rows: games } = await connection.query(defaultQuery)
+        return res.status(200).send(games)
     } catch (error) {
         console.error(error)
         res.sendStatus(500)        
